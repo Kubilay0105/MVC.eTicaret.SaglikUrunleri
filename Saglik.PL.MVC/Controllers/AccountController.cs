7cd167aded7db2ceb8fc19468b72eb0db6f91be0
Kubilay <kubilay__1903@hotmail.com>
2019-04-29 01:03:41 +0300

    tamamlandı login logout

--- Saglik.PL.MVC/Controllers/AccountController.cs
+++ Saglik.PL.MVC/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
 using Saglik.BLL.Identity;
 using Saglik.BLL.Repository;
 using Saglik.DAL.Context;
@@ -8,6 +9,7 @@ using Saglik.PL.MVC.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -18,16 +20,14 @@ namespace Saglik.PL.MVC.Controllers
         Repository<Uye> URepo = new Repository<Uye>(new SaglikContext());
         // GET: Account
         public ActionResult Register()
-        {
-            ApplicationRole role = new ApplicationRole();
-            role.Name = "User";
-            var rolemanager = IdentityTools.NewRoleManager();
-            rolemanager.Create(role);
+        {         
             return View();
         }
         [HttpPost]
         public ActionResult Register(RegisterVM model)
         {
+            if (!ModelState.IsValid)
+                return View();
             var usermanager = IdentityTools.NewUserManager();
             var kullanici = usermanager.FindByEmail(model.Email);
             if (kullanici != null)
@@ -39,6 +39,7 @@ namespace Saglik.PL.MVC.Controllers
             user.Email = model.Email;
             user.UserName = model.Email;
             user.Tarih = model.Tarih;
+
             var result = usermanager.Create(user, model.Password);
 
             if (result.Succeeded)
@@ -55,5 +56,40 @@ namespace Saglik.PL.MVC.Controllers
             }
             return View(model);
         }
+        [HttpPost]
+        public ActionResult Login(string username,string password)
+        {
+            var usermanager = IdentityTools.NewUserManager();
+            var kullanici = usermanager.FindByEmail(username);
+            if (kullanici == null)
+            {
+                ModelState.AddModelError("", " *Böyle bir kullanıcı kayıtlı değil!");
+                return View();
+            }
+            else
+            {
+                if (!usermanager.CheckPassword(kullanici, password))
+                {
+                    ModelState.AddModelError("", " *Şifre Hatalı!");
+                    return View();
+                }
+                var authManager = HttpContext.GetOwinContext().Authentication;
+                var identity = usermanager.CreateIdentity(kullanici, "ApplicationCookie");
+                var authProperty = new AuthenticationProperties
+                {
+                    IsPersistent = false
+                };
+                authManager.SignIn(authProperty, identity);
+                return RedirectToAction("Index", "Home");
+
+            }
+        }
+        [Authorize]
+        public ActionResult LogOut()
+        {
+            HttpContext.GetOwinContext().Authentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
     }
+   
 }
---
+++ Saglik.PL.MVC/Models/LoginVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Saglik.PL.MVC.Models
+{
+    public class LoginVM
+    {
+        [Required]
+        [Display(Name = "Kullanıcı Adı")]
+        public string Username { get; set; }
+        [Display(Name = "Şifre")]
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public string returnUrl { get; set; }
+    }
+}
